6d258ee13e3b3d98bb41b792a790916dd3617e28
daxwann <daxwann@gmail.com>
2018-12-26 16:13:38 -0800

    modify header

--- fillit.h
+++ fillit.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xwang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/26 16:12:21 by xwang             #+#    #+#             */
+/*   Updated: 2018/12/26 16:12:56 by xwang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FILLIT_H
 # define FILLIT_H
 # include <string.h>
@@ -35,30 +47,24 @@ typedef struct		s_piece
 	int				top;
 }					t_piece;
 
-/* output */
 void				ft_putchar(char c);
 size_t				ft_strlen(const char *str);
 void				ft_putstr(const char *str);
 int					display_usage_error(int error_code);
-/* list */
 t_list				*ft_lstnew(void *tet_piece);
 t_list				*ft_lstpush(t_list **head, t_list *last, t_list *new_ele);
 t_list				*create_list(int fd, char *buf);
-/* board */
 int					get_board_size(int pieces);
 void				initialize_board(char board[104][104]);
 void				print_board(char board[104][104], int size);
-/* piece */
 int					count_pieces(t_list *tetriminos);
 void				initialize_piece(t_piece *piece);
 int					count_connections(char *str, int position);
 void				modify_piece(t_piece *piece, char *str, int row, int pos);
-/* tetrimino */
 t_tet				*new_tet(char *str, int top, int left, int count);
 void				add_tet(t_board *bd, int row, int col, t_tet *tet);
 void				delete_tet(t_board *bd, int row, int col, t_tet *tet);
 t_tet				*check_tet(char *str, int count);
-/* backtracking */
 int					fits_on_board(t_board *bd, t_tet *tet, int row, int col);
 int					next_tet(t_board *bd, t_list *tetriminos);
 int					solve(t_board *bd, t_list *tetriminos);
